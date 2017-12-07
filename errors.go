7495ea3d18de5d3d9d4d97b0ea75ec09e39d4b5c
ShuyueZhou <free1139@163.com>
2017-12-07 22:35:05 +0800

    调整文档描述为中文，以便更加能描述出设计的思路

--- errors.go
+++ errors.go
@@ -1,3 +1,36 @@
+//
+// 错误记录器
+// 本设计补充并实现了系统的error接口，
+// 用于发生错误时附带发生错误的原因、位置等信息以便还原现场。
+// 因本错误设计含有比较大的数据量信息，因此不应用于正常的逻辑场景，以避免影响到程序效率。
+//
+// 使用例子
+// package main
+//
+// import "git.ot24.net/ot24net/errors"
+//
+// func fn1(a int) error {
+//    if a == 1{
+//        return errors.ErrNoData.As(a)
+//    }
+//    return errors.New("not implements").As(a)
+// }
+//
+// func fn2(b int) error {
+//    return errors.As(fn1(b))
+// }
+//
+// func main() {
+//    err := fn2(2)
+//    if err != nil {
+//        if !errors.ErrNoData.Equal(err) {
+//            panic(err)
+//	      }
+//        // 处理错误码相等的情况
+//        fmt.Println(err)
+//	  }
+// }
+//
 package errors
 
 import (
@@ -12,46 +45,50 @@ var (
 )
 
 type Error interface {
-	// extends for error interface.
+	// Extends for error interface.
 	Error() string
-	// extends for json custom interface,
-	// and it can be use as value for errors.As().
+	// Extends for json custom interface,
+	// and it will be used as value data in errors.As().
 	MarshalJSON() ([]byte, error)
 
-	// return error code.
+	// Return error code.
 	Code() string
 
 	// Add reason and caller positoin for error.
 	//
-	// call as :
+	// Call as :
 	// err.As(reason)
 	// or errors.As(err, reason)
 	//
-	// append reason and return it self
+	// Append reason and return it self
 	As(arg ...interface{}) Error
 
 	// Compare error code
-	// call as :
+	// Aall as :
 	// err.Equal(anotherErr)
 	// or errors.Equal(err)
 	Equal(err error) bool
 }
 
-// Equal
-// compare code with err1 and err2.
 //
-// Param
-// err1 -- first error which want to compare.
-// err2 -- another error which want to compare.
+// 比较两个错误的值是否相等.
+// 该比较有两个范围，
+// 一个是内存是否相等，常用于同个程序中产生的错误的比较，若内存相等，则两个错误是相等的;
+// 一个是值是否相等，常用于跨程序中产生的错误比较，若不是此接口的Error，则比较Error()的值是否相等；
+// 若属于此Error接口, 则比较Code()的值是否相等，
 //
-// Return
-// return the result of comparing.
+// 参数
+// err1 -- 错误1
+// err2 -- 另一个需要比较的错误
+//
+// 返回
+// 返回是否相等，true相等
 func Equal(err1 error, err2 error) bool {
 	return equal(err1, err2)
 }
 
 func equal(err1 error, err2 error) bool {
-	// memory compare
+	// Memory compare
 	if err1 == err2 {
 		return true
 	}
@@ -73,40 +110,51 @@ type errImpl struct {
 	data ErrData
 }
 
-// New
-// create an Error which it is implemented error interface.
 //
-// Param
-// code -- code or msg for the error keycode.
+// 创建一个本包Error接口的实例
 //
-// Return
-// return a Error interface
+// 参数
+// code -- 错误码或者文字描述，此值将用于Equal的比较
+//
+// 返回
+// 返回Error实例
 func New(code string) Error {
 	return &errImpl{
 		ErrData{
 			Code:   code,
-			Reason: [][]interface{}{{"new"}},
+			Reason: [][]interface{}{{"init"}},
 			Where:  []string{caller(2)},
 		},
 	}
 }
 
-// ParseError
-// Parse a standar error to this Error interface.
-// if the parameter is belong to this Error, do a value copy an return a new Err.
-// or parse string with error.Error(),
-// if the string have a json struct with Err.Error(),return the origin struct with a new Err.
-// or using error.Error() to create a new Err.
 //
-// Spec
-// in the two case before, it will keep the key same as origin.
-// the location is not change in parsing.
+// 解析一个错误文本
+// 通常它是一个本包的Error()序列化数据, 该数据是一个json数据，将直接被序列化为本包的Error类型;
+// 若非本包的接口的序列化结构，将被直接New一个新的Error出来
 //
-// Param
-// src -- any error who implement error interface.
+// 参数
+// src -- 需要解析的文本
 //
-// Return
-// return a new Error interface.
+// 返回
+// 返回Error实例
+func Parse(src string) Error {
+	if len(src) == 0 {
+		return nil
+	}
+	return parse(src)
+}
+
+//
+// 将一个标准的错误转为本包的Error接口类型
+// 若该错误本已经是本包的Error类型，则直接转为本包的Error并返回;
+// 若该错误是非本包的Error类型，则调用error.Error()进行值解析创建一个新的本包Error
+//
+// 参数
+// src -- 错误来源
+//
+// 返回
+// 返回本实例的Error实例
 func ParseError(src error) Error {
 	if src == nil {
 		return nil
@@ -117,25 +165,17 @@ func ParseError(src error) Error {
 	return parse(src.Error())
 }
 
-func Parse(src string) Error {
-	if len(src) == 0 {
-		return nil
-	}
-	return parse(src)
-}
-
-// As
-// Parse the error, and fix with reason,it can make a replenishment for a same error.
 //
-// Spec
-// because the value of error is change, so that location of Where is changed.
+// 给一个错误构建错误定位信息
+// 解析error时等价于ParseError，并在解析出的Error后构建当前置的错误定位信息。
+// 若解析出的是本包的Error类型的实现，将在原实现基础上构建错误定位信息，此时也等价于Error的As方法调用。
 //
-// Param
-// err -- any error interface
-// reason -- a array reason,it will be append to the reason of parameter.
+// 参数
+// err -- 任意类型的error实现
+// reason -- 错误的原因，通常是引起发生错误的参数，以便记录并还原出发生错误时的调用。
 //
-// Return
-// return a New Err,but with a same key with param error.
+// 返回
+// 返回增加了定位信息的Error实现
 func As(err error, reason ...interface{}) Error {
 	if err == nil {
 		return nil
@@ -192,10 +232,12 @@ func caller(depth int) string {
 	return at
 }
 
+// Error的Code方法实现
 func (e *errImpl) Code() string {
 	return e.data.Code
 }
 
+// Error的Error方法实现
 func (e *errImpl) Error() string {
 	data, err := json.Marshal(e.data)
 	if err != nil {
@@ -204,14 +246,17 @@ func (e *errImpl) Error() string {
 	return string(data)
 }
 
+// Error的MarshalJson方法实现
 func (e *errImpl) MarshalJSON() ([]byte, error) {
 	return json.Marshal(e.data)
 }
 
+// Error的Equal方法实现
 func (e *errImpl) Equal(l error) bool {
 	return equal(e, l)
 }
 
+// Error的As方法实现
 func (e *errImpl) As(reason ...interface{}) Error {
 	e.data.Reason = append(e.data.Reason, reason)
 	e.data.Where = append(e.data.Where, caller(2))
